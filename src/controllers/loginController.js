e75d4be5a24700eadfdf72b0eab5cf84752c0f39
Vinicius Frota <viniciussantosfrota10012017@gmail.com>
2024-12-05 18:06:30 -0300

    Parte 2

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,3 +1,61 @@
+const Login = require('../models/LoginModel')
+
 exports.index = (req, res) => {
-    res.render('login')
+    if(req.session.user) return res.render('login-logged')
+    return res.render('login')
+}
+
+exports.register = async function (req, res) {
+    try {
+        const login = new Login(req.body)
+        await login.register()
+        
+        if(login.error.length > 0){
+            req.flash('errors', login.errors)
+            req.session.save(function(){
+                return res.redirect('back')
+            })
+            return
+        }
+
+        req.flash('sucess', 'Seu usuário foi criado com sucesso')
+        req.session.save(function(){
+            return res.redirect('back')
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.render('./includes/404')
+    }
+}
+
+exports.login = async function (req, res) {
+    try {
+        const login = new Login(req.body)
+        await login.login()
+        
+        if(login.error.length > 0 ){
+            req.flash('errors', login.error)
+            req.session.save(()=>{
+                return res.redirect('back')
+            })
+            return
+        }
+
+        req.flash('sucess', 'Você entrou no sistema')
+        req.session.user = login.user
+        req.session.save(()=>{
+            return res.redirect('back')
+        })
+
+
+    } catch (error) {
+        console.log(error)
+        return res.render('.includes/404')
+    }
+}
+
+exports.logout = function(req, res) {
+    res.session.destroy()
+    res.redirect('/')
 }
--- src/middleware/globarMiddleware.js
+++ src/middleware/globarMiddleware.js
@@ -1,5 +1,7 @@
 exports.middlewareGlobal = (req, res, next) =>{
-    res.locals.OneLocalVar = ''
+    res.locals.erros = req.flash('erros')
+    res.locals.sucess = req.flash('sucess')
+    res.locals.user = req.session.user;
     next()
 }
 
@@ -7,9 +9,21 @@ exports.checkCsrfTokenError = (err, req, res, next) =>{
     if(err){
         return res.render('404')
     }
+    next()
 }
 
 exports.csrfMiddleware = (req, res, next) =>{
     res.locals.csrfToken = req.csrfToken()
+    next()
+}
+
+exports.loginRequired = (req, res, next) =>{
+    if(!req.session.user){
+        req.flash('errors', 'Você precisa fazer o login.')
+        req.session.save(()=>{
+            res.redirect('/')
+        })
+        return
+    }
     next()
 }
